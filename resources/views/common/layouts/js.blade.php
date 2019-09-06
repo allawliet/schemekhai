3f89036944a758aee2234a6db02e734db1a097fb
allawliet <alqiz98@gmail.com'git config --global user.name allawliet>
2019-09-06 12:00:46 +0800

    plus minus button for collapse

--- resources/views/common/layouts/js.blade.php
+++ resources/views/common/layouts/js.blade.php
@@ -212,4 +212,21 @@
         ]
     });
     $('.buttons-copy, .buttons-csv, .buttons-print, .buttons-pdf, .buttons-excel').addClass('btn btn-primary mr-1');
+
+    
+    //mat-collapse plus minus
+    $(document).ready(function(){
+    // Add minus icon for collapse element which is open by default
+    $(".collapse.show").each(function(){
+    $(this).prev(".card-header").find(".fa").addClass("fa-minus").removeClass("fa-plus");
+    });
+    
+    // Toggle plus minus icon on show hide of collapse element
+    $(".collapse").on('show.bs.collapse', function(){
+    $(this).prev(".card-header").find(".fa").removeClass("fa-plus").addClass("fa-minus");
+    }).on('hide.bs.collapse', function(){
+    $(this).prev(".card-header").find(".fa").removeClass("fa-minus").addClass("fa-plus");
+    });
+});
+
     </script>
